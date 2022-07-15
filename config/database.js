df9de452b788572b0956926d4dc51213d319282c
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2022-07-15 12:07:19 +0200

    db fix

--- config/database.js
+++ config/database.js
@@ -7,16 +7,12 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', ''),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        database: env('DATABASE_NAME', 'my-strapi-project'),
+        username: env('DATABASE_USERNAME', 'testing'),
+        password: env('DATABASE_PASSWORD', 'testing'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {
-        ssl: true,
-      },
+      options: {}
     },
   },
 });
