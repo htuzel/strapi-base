b951937da81225a6a7ebefb12c6b0d6cd598a2b9
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2022-07-15 10:20:52 +0200

    first commit

---
+++ config/database.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'forge'),
+        password: env('DATABASE_PASSWORD', 'bzPEgQApY9fUqRVzWqTi'),
+      },
+      options: {
+        ssl: false,
+      },
+    },
+  },
+});
