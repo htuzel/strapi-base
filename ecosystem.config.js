10f3be7108cf153644730bc090a2f4953778f615
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2022-07-15 15:20:38 +0200

    new env values

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,12 @@ module.exports = {
       DATABASE_PORT: '25060',
       DATABASE_NAME: 'strapi', // DB name
       DATABASE_USERNAME: 'forge', // your username for psql
-      DATABASE_PASSWORD: 'aM6fNpTqhCBJXlaE' // your password for psql
+      DATABASE_PASSWORD: 'aM6fNpTqhCBJXlaE', // your password for psql
+      PORT: '1337',
+      APP_KEYS: 'gZMvdFpMZtTAPw/UKnSrYg==,BGL70ZOdy+5+KqWJmKAp1Q==,Ridf9FBCVV5Bzl34w0GaDQ==,6eG411y3A/z3BDKd3cWxNQ==',
+      API_TOKEN_SALT: 'HzLbgNpwmWWtkl6oYkkaDw==',
+      ADMIN_JWT_SECRET: 'R9S/AMcXQKFt1XYnDrLAHg==',
+      JWT_SECRET: 'zkSyqIEoAjzycpOadGTn3w=='
     },
   }, ],
 };
