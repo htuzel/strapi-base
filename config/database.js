f98db2c148efc88b3f39ca997c096643b969f289
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2022-07-15 12:37:44 +0200

    fix

--- config/database.js
+++ config/database.js
@@ -6,14 +6,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql2',
     connection: {
-      host: env('DATABASE_HOST', 'db-mysql-fra1-get-do-user-3818724-0.b.db.ondigitalocean.com'),
+      host: env('DATABASE_HOST', 'private-db-mysql-fra1-get-do-user-3818724-0.b.db.ondigitalocean.com'),
       port: env.int('DATABASE_PORT', 25060),
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'doadmin'),
       password: env('DATABASE_PASSWORD', 'VgcvGfeaDS5IWE4b'),
-      ssl: {
-        ca: fs.readFileSync(`${__dirname}/ca-certificate.crt`).toString(),
-      },
+      ssl: true
     },
     debug: false,
   },
