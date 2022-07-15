e75cc88803f1348d6c5856ffc3d146cad3c46da3
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2022-07-15 12:02:52 +0200

    database update

--- config/database.js
+++ config/database.js
@@ -10,9 +10,12 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', ''),
         username: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       options: {
-        ssl: false,
+        ssl: true,
       },
     },
   },
